229a26518efcecadacc8425ea7e1a82c67b4c67c
tkarlinski <karlinski.tomasz@gmail.com>
2017-08-03 23:49:19 +0200

    changed location cache and logs

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,6 +5,17 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
+    const ENV_DEVELOPMENT = 'dev';
+    const ENV_TESTING = 'test';
+    const ENV_ACCEPTANCE = 'rc';
+    const ENV_PRODUCTION = 'prod';
+
+    const DIR_VAR = 'var';
+    const DIR_CACHE = 'cache';
+    const DIR_LOGS = 'logs';
+    const DIR_TEMPORARY = 'tmp';
+    const DIR_UPLOADS = 'uploads';
+
     public function registerBundles()
     {
         $bundles = array(
@@ -16,7 +27,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
+//            new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
@@ -31,6 +42,54 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    }
+
+    /**
+     * Zwraca ściężkę do zapisu plików
+     *
+     * @return string
+     */
+    protected function _getStorageDirectory()
+    {
+        return dirname($this->getRootDir()) . '/' . self::DIR_VAR . '/' . $this->getEnvironment();
+    }
+
+    /**
+     * Zwraca ścieżkę do katalogu tymczasowego aplikacji
+     *
+     * @return string
+     */
+    public function getTempDir()
+    {
+        return $this->_getStorageDirectory() . '/' . self::DIR_TEMPORARY;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @see \Symfony\Component\HttpKernel\Kernel::getCacheDir()
+     */
+    public function getCacheDir()
+    {
+        return $this->_getStorageDirectory() . '/' . self::DIR_CACHE;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @see \Symfony\Component\HttpKernel\Kernel::getLogDir()
+     */
+    public function getLogDir()
+    {
+        return $this->_getStorageDirectory() . '/' . self::DIR_LOGS;
+    }
+
+    /**
+     * Zwraca ścieżkę do katalogu, w którym są uploadowane pliki
+     *
+     * @return string
+     */
+    public function getUploadDir()
+    {
+        return $this->_getStorageDirectory() . '/' . self::DIR_UPLOADS;
     }
 }
